a3c1023c9445fbbdaac837eedfe64217fa384cc3
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2019-07-11 14:01:31 +0530

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+import Control.Monad.State
+import Lib
+import Stack
+
+main :: IO ()
+main = print $ evalState program []
---
+++ src/Stack.hs
@@ -0,0 +1,27 @@
+module Stack where
+
+import Control.Monad.State
+
+type Stack = [Int]
+
+empty :: Stack
+empty = []
+
+pop :: State Stack Int
+pop = state $ \(x : xs) -> (x, xs)
+
+push :: Int -> State Stack ()
+push n = state $ \xs -> ((), n : xs)
+
+peek :: State Stack Int
+peek = state $ \(x : xs) -> (x, x : xs)
+
+program :: State Stack Int
+program = do
+    push 10
+    push 20
+    push 30
+    a <- pop
+    b <- pop
+    push (a + b)
+    peek
